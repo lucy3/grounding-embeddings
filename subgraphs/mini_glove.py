a5e7c3a9ec5e8478abb83430727c2b6f16ee634f
lucy3 <luxisufeili@gmail.com>
2017-03-05 21:44:55 +0000

    fruit and veg graph for glove 300d wikipedia can be created using both SNAP and NetworkX

---
+++ subgraphs/mini_glove.py
@@ -0,0 +1,43 @@
+"""
+Using NetworkX
+
+This creates a small fruit/veg subgraph based
+on cosine similarities of words in glove.6B.300d.txt.
+
+- input: cosine simlarities and vocabulary
+- output: graph png
+
+"""
+
+import networkx as nx
+from collections import defaultdict
+from nxpd import draw
+
+INPUT_FILE = "fruitveg_sim.txt"
+VOCAB = "mcrae_fruitveg.txt"
+OUTPUT_GRAPH = "fruitveg_out.png"
+
+def get_cosine_dist():
+	d = defaultdict(float)
+	word_sim = open(INPUT_FILE, 'r')
+	for line in word_sim:
+		pair = tuple(line.split()[:2])
+		dist = float(line.split()[2])
+		d[pair] = dist
+	return d
+
+def main():
+	cosine_dists = get_cosine_dist()
+
+	g = nx.Graph()
+	vocab_file = open(VOCAB, 'r')
+	for line in vocab_file:
+		g.add_node(line.strip())
+	for pair in cosine_dists:
+		if cosine_dists[pair] > 0.5:
+			g.add_edge(pair[0], pair[1])
+
+	draw(g, filename=OUTPUT_GRAPH)
+
+if __name__ == '__main__':
+	main()
---
+++ subgraphs/mini_glove_dist.py
@@ -0,0 +1,38 @@
+"""
+This computes cosine distances for a subset of fruits/vegs.
+
+mcrae_fruitveg.txt is hand-picked list of fruits/vegs
+from the McRae concept dataset.
+
+- input: vocabulary, glove data
+- output: cosine similarities in a txt
+"""
+
+from collections import defaultdict
+from scipy import spatial
+
+GLOVE_INPUT = "../glove/glove.6B.300d.txt"
+VOCAB = "mcrae_fruitveg.txt"
+OUTPUT = "fruitveg_sim.txt"
+
+def main():
+	vocab_file = open(VOCAB, 'r')
+	vocabulary = set()
+	for line in vocab_file:
+		vocabulary.add(line.strip())
+	f = open(GLOVE_INPUT, 'r')
+	vectors = defaultdict(list)
+	for line in f:
+		word_vec = line.split()
+		if word_vec[0] in vocabulary:
+			vectors[word_vec[0]] = [float(x) for x in word_vec[1:]]
+
+	output = open(OUTPUT, 'w')
+	words = vectors.keys()
+	for i in range(len(words)):
+		for j in range(i+1, len(words)):
+			dist = 1- spatial.distance.cosine(vectors[words[i]], vectors[words[j]])
+			output.write(words[i] + ' ' + words[j] + ' ' + str(dist) + '\n')
+
+if __name__ == '__main__':
+	main()
