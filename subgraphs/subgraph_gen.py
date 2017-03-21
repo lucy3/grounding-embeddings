abf37d062780ec854837ef3a75c1a958cb27b613
lucy3 <luxisufeili@gmail.com>
2017-03-21 13:46:41 -0500

    COMMUNITIES

--- subgraphs/subgraph_gen.py
+++ subgraphs/subgraph_gen.py
@@ -22,14 +22,16 @@ import operator
 NUM_EDGES = 200
 
 # uncomment for GloVe data
-# INPUT_FILE = "./fruitveg/fruitveg_sim_glove.txt"
-# VOCAB = "./fruitveg/vocab_fruitveg.txt"
-# OUTPUT_GRAPH = "./fruitveg/fruitveg_out_glove" + str(NUM_EDGES) + ".png"
+INPUT_FILE = "./fruitveg/fruitveg_sim_glove.txt"
+VOCAB = "./fruitveg/vocab_fruitveg.txt"
+OUTPUT_GRAPH = "./fruitveg/fruitveg_out_glove" + str(NUM_EDGES) + ".png"
+OUTPUT_STATS = "./fruitveg/fruitveg_glove_stats.txt"
 
 # uncomment for McRae data
-INPUT_FILE = "./fruitveg/fruitveg_sim_mcrae.txt"
-VOCAB = "./fruitveg/vocab_fruitveg.txt"
-OUTPUT_GRAPH = "./fruitveg/fruitveg_out_mcrae" + str(NUM_EDGES) + ".png"
+# INPUT_FILE = "./fruitveg/fruitveg_sim_mcrae.txt"
+# VOCAB = "./fruitveg/vocab_fruitveg.txt"
+# OUTPUT_GRAPH = "./fruitveg/fruitveg_out_mcrae" + str(NUM_EDGES) + ".png"
+# OUTPUT_STATS = "./fruitveg/fruitveg_mcrae_stats.txt"
 
 def get_cosine_dist():
 	d = defaultdict(float)
@@ -39,6 +41,31 @@ def get_cosine_dist():
 		dist = float(line.split()[2])
 		d[pair] = dist
 	return d
+
+def output_graph_stats(g):
+	stat_file = open(OUTPUT_STATS, 'w')
+	stat_file.write("NOTE: graph is treated as an unweighted graph" + "\n\n")
+	stat_file.write(str(nx.info(g)) + "\n\n")
+	stat_file.write("TRANSITIVITY: " + str(nx.transitivity(g)) + "\n\n")
+	clust_coeffs = nx.clustering(g)
+	stat_file.write("NODES WITH CLUST COEFF = 1: " + "\n")
+	# there should be better ways to find cliques
+	for node in clust_coeffs:
+		if clust_coeffs[node] == 1.0:
+			stat_file.write(node + " " + str(g.neighbors(node)) + "\n")
+	stat_file.write("AVG CLUSTERING COEFFICIENT: " +
+		str(nx.average_clustering(g)) + "\n\n")
+	stat_file.write("DEGREE HISTOGRAM: " + str(nx.degree_histogram(g)) + "\n\n")
+	stat_file.write("NODES WITH HIGHEST DEGREE CENTRALITY: " + "\n")
+	stat_file.write(str(sorted(nx.degree_centrality(g).items(),
+		key=operator.itemgetter(1), reverse=True)[:5]) + "\n\n")
+	stat_file.write("4-CLIQUE COMMUNITIES (clique percolation): " + "\n")
+	for clique in nx.k_clique_communities(g, 4):
+		stat_file.write(str(clique) + "\n")
+	stat_file.write("\nMAXIMAL CLIQUES: " + "\n")
+	for clique in nx.find_cliques(g):
+		if len(clique) >= 3:
+			stat_file.write(str(clique) + "\n")
 
 def main():
 	cosine_dists = get_cosine_dist()
@@ -56,6 +83,7 @@ def main():
 		g[pair[0]][pair[1]]["w"] = tup[1]
 
 	draw(g, filename=OUTPUT_GRAPH)
+	output_graph_stats(g)
 
 if __name__ == '__main__':
 	main()
