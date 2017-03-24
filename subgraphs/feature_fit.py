ba22aeb4e498ce273e87c478b19bdae201358015
Jon Gauthier <jon@gauthiers.net>
2017-03-24 14:37:30 -0700

    Draw a little more meta-info for features from McRae data.

--- subgraphs/feature_fit.py
+++ subgraphs/feature_fit.py
@@ -1,5 +1,5 @@
 import codecs
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from pathlib import Path
 
 import numpy as np
@@ -14,7 +14,11 @@ EMBEDDINGS = "./all/embeddings.%s.npy" % EMBEDDING_NAME
 OUTPUT = "./all/feature_fit.txt"
 
 
-def load_embeddings(c2f):
+Feature = namedtuple("Feature", ["name", "concepts", "wb_label", "wb_maj",
+                                 "wb_min", "br_label", "disting"])
+
+
+def load_embeddings(concepts):
     if Path(EMBEDDINGS).is_file():
         embeddings = np.load(EMBEDDINGS)
 
@@ -28,7 +32,7 @@ def load_embeddings(c2f):
             for line in glove_f:
                 fields = line.strip().split()
                 word = fields[0]
-                if word in c2f:
+                if word in concepts:
                     vec = [float(x) for x in fields[1:]]
                     embeddings.append(vec)
                     vocab.append(word)
@@ -45,33 +49,38 @@ def load_embeddings(c2f):
 def load_features_concepts():
     """
     Returns:
-        c2f: dict concept -> set(features)
-        f2c: dict feature -> set(concepts)
+        features: string -> Feature
+        concepts: set of strings
     """
-    c2f, f2c = defaultdict(set), defaultdict(set)
+    features = {}
+    concepts = set()
 
     with open(FEATURES, "r") as features_f:
         for line in features_f:
-            concept, feature = line.strip().split("\t")[:2]
+            fields = line.strip().split("\t")
+            concept_name, feature_name = fields[:2]
+            if feature_name not in features:
+                features[feature_name] = Feature(
+                        feature_name, set(), *fields[2:6], fields[10])
 
-            c2f[concept].add(feature)
-            f2c[feature].add(concept)
+            features[feature_name].concepts.add(concept_name)
+            concepts.add(concept_name)
 
-    lengths = [len(xs) for xs in f2c.values()]
+    lengths = [len(f.concepts) for f in features.values()]
     from collections import Counter
     from pprint import pprint
     print("# of features with particular number of associated concepts:")
     pprint(Counter(lengths))
 
-    return c2f, f2c
+    return features, concepts
 
 
-def analyze_feature(feature, f2c, word2idx, embeddings):
+def analyze_feature(feature, features, word2idx, embeddings):
     # Fetch available embeddings.
     embeddings = [embeddings[word2idx[concept]]
-                  for concept in f2c[feature]
+                  for concept in features[feature].concepts
                   if concept in word2idx]
-    if len(embeddings) <= 1:
+    if len(embeddings) < 4 or len(embeddings) > 7:
         return
     embeddings = np.asarray(embeddings)
     embeddings /= np.linalg.norm(embeddings, axis=1, keepdims=True)
@@ -83,12 +92,12 @@ def analyze_feature(feature, f2c, word2idx, embeddings):
 
 
 def main():
-    c2f, f2c = load_features_concepts()
-    vocab, embeddings = load_embeddings(c2f)
+    features, concepts = load_features_concepts()
+    vocab, embeddings = load_embeddings(concepts)
     word2idx = {w: i for i, w in enumerate(vocab)}
 
-    feature_data = [analyze_feature(feature, f2c, word2idx, embeddings)
-                    for feature in f2c]
+    feature_data = [analyze_feature(feature, features, word2idx, embeddings)
+                    for feature in features]
     feature_data = sorted(filter(None, feature_data), key=lambda f: f[2])
 
     for name, n_entries, score in feature_data:
