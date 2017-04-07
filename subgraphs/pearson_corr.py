eadadeae20d37df89cdb2739cf8418c932c05c82
Jon Gauthier <jon@gauthiers.net>
2017-04-06 18:05:13 -0700

    pearson_corr: default to wikigiga as GloVe source

--- subgraphs/pearson_corr.py
+++ subgraphs/pearson_corr.py
@@ -25,8 +25,9 @@ from nltk.corpus import wordnet as wn
 
 VOCAB = "./all/vocab.txt"
 INPUT_FILE1 = "./all/sim_mcrae.txt"
-INPUT_FILE2 = "./all/sim_glove_tw.txt"
-OUTPUT_FILE = "./all/pearson_corr/corr_mcrae_tw.txt"
+# INPUT_FILE2 = "./all/sim_glove_tw.txt"
+INPUT_FILE2 = "./all/sim_glove.txt"
+OUTPUT_FILE = "./all/pearson_corr/corr_mcrae_wikigiga.txt"
 CONC_BRM = "../mcrae/CONCS_brm.txt"
 CONCSTATS = "../mcrae/CONCS_FEATS_concstats_brm.txt"
 DOMAINS = set(["a_bird", "a_fish", "a_fruit", "a_mammal", \
