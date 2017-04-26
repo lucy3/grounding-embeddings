59d7045e853dfac2ab697f665eaf480575a5060f
lucy3 <luxisufeili@gmail.com>
2017-04-26 10:49:41 -0700

    basic counts

--- subgraphs/mini_cslb_dist.py
+++ subgraphs/mini_cslb_dist.py
@@ -1,11 +1,11 @@
 """
-CSLB: get similarities with LSA'd vectors
+CSLB: doesn't actually calculate distances 
 """
 import numpy as np
 
 GLOVES = ["../glove/glove.6B.300d.txt", "../glove/glove.840B.300d.txt"]
 FEATURE_CONCEPTS = "../cslb/feature_matrix.dat"
-VOCAB = "./all/vocab_cslb.txt"
+VOCAB = "./all/vocab_cslb.txt" # OUTPUT
 
 def create_vocab_file(vocab):
 	print(len(vocab))
@@ -22,7 +22,12 @@ def create_vocab_file(vocab):
 		vocab_file.write(word + "\n")
 
 def process_feature_concepts():
+	vocab_file = open(VOCAB, 'r')
+	vocabulary = set()
+	for line in vocab_file:
+		vocabulary.add(line.strip())
 	# vocab = set()
+	ours = []
 	f_per_c = []
 	with open(FEATURE_CONCEPTS, 'r') as file:
 		features = file.readline().split()[1:]
@@ -31,6 +36,8 @@ def process_feature_concepts():
 			l = line.split()
 			concept = l[0]
 			features = np.array([float(x) for x in l[1:]])
+			if concept in vocabulary:
+				ours.append(features)
 			f_per_c.append(np.count_nonzero(features))
 			for i in range(len(features)):
 				if features[i] != 0:
@@ -50,16 +57,34 @@ def process_feature_concepts():
 	print("num features total")
 	print(len(c_per_f))
 
+	f_per_c = []
+	ours = np.array(ours)
+	ours = ours[:,~np.all(ours == 0, axis=0)]
+	c_per_f = np.zeros(ours.shape[1])
+	for c in ours:
+		f_per_c.append(np.count_nonzero(c))
+		for i in range(len(c)):
+			if c[i] != 0:
+				c_per_f[i] += 1
+	print("# features per concept:")
+	print("mean\tmedian\tmin\tmax")
+	print("%f %f %f %f" % (np.mean(f_per_c), np.median(f_per_c), np.min(f_per_c),
+		np.max(f_per_c)))
+	print("# concepts per feature")
+	print("mean\tmedian\tmin\tmax")
+	print("%f %f %f %f" % (np.mean(c_per_f), np.median(c_per_f), np.min(c_per_f),
+		np.max(c_per_f)))
+	print("num features with >= 5 concepts")
+	print(sum(i >= 5 for i in c_per_f))
+	print("num features total")
+	print(len(c_per_f))
+
+
 	#create_vocab_file(vocab)
-
-
-def preliminaries():
-	pass
 
 
 def main():
 	process_feature_concepts()
-	preliminaries()
 
 if __name__ == '__main__':
 	main()
