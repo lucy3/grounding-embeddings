53de647be9bb1428148d45706bc6e5dba963668b
lucy3 <luxisufeili@gmail.com>
2017-04-26 15:49:26 -0700

    edited load_features_concepts. CSLB does not have several Feature attributes. also CV code was uncommented so I did not run

--- subgraphs/feature_fit.py
+++ subgraphs/feature_fit.py
@@ -39,7 +39,10 @@ elif PIVOT == "cc":
     INPUT = "../glove/glove.840B.300d.txt"
 
 SOURCE = "mcrae"
-FEATURES = "../mcrae/CONCS_FEATS_concstats_brm.txt"
+if SOURCE == "mcrae":
+    FEATURES = "../mcrae/CONCS_FEATS_concstats_brm.txt"
+else:
+    FEATURES = "../cslb/norms.dat"
 VOCAB = "./all/vocab_%s.txt" % SOURCE
 EMBEDDINGS = "./all/embeddings.%s.%s.npy" % (SOURCE, PIVOT)
 
@@ -96,25 +99,42 @@ def load_features_concepts():
     features = {}
     concepts = set()
 
-    with open(FEATURES, "r") as features_f:
-        for line in features_f:
-            fields = line.strip().split("\t")
-            concept_name, feature_name = fields[:2]
-            if concept_name == "Concept" or concept_name == "dunebuggy":
-                # Header row / row we are going to ignore!
-                continue
-            if feature_name not in features:
-                features[feature_name] = Feature(
-                        feature_name, set(), *fields[2:6], fields[10])
+    if SOURCE == "mcrae":
+        with open(FEATURES, "r") as features_f:
+            for line in features_f:
+                fields = line.strip().split("\t")
+                concept_name, feature_name = fields[:2]
+                if concept_name == "Concept" or concept_name == "dunebuggy":
+                    # Header row / row we are going to ignore!
+                    continue
+                if feature_name not in features:
+                    features[feature_name] = Feature(
+                            feature_name, set(), *fields[2:6], fields[10])
 
-            features[feature_name].concepts.add(concept_name)
-            concepts.add(concept_name)
+                features[feature_name].concepts.add(concept_name)
+                concepts.add(concept_name)
 
-    lengths = [len(f.concepts) for f in features.values()]
-    from collections import Counter
-    from pprint import pprint
-    print("# of features with particular number of associated concepts:")
-    pprint(Counter(lengths))
+        lengths = [len(f.concepts) for f in features.values()]
+        from collections import Counter
+        from pprint import pprint
+        print("# of features with particular number of associated concepts:")
+        pprint(Counter(lengths))
+
+    if SOURCE == "cslb":
+        with open(FEATURES, "r") as features_f:
+            reader = csv.DictReader(features_f, delimiter='\t')
+            for row in reader:
+                concept_name = row["concept"]
+                feature_name = "_".join(row["feature"].split())
+                if feature_name not in features:
+                    features[feature_name] = Feature(feature_name, set(),
+                        "", "", "", row["feature type"], "")
+
+                features[feature_name].concepts.add(concept_name)
+                concepts.add(concept_name)
+        lengths = [len(f.concepts) for f in features.values()]
+        print("# of features with particular number of associated concepts:")
+        pprint(Counter(lengths))
 
     return features, concepts
 
@@ -232,16 +252,6 @@ def analyze_features(features, word2idx, embeddings):
     return zip(feature_names, counts, ret_metrics)
 
 
-def produce_domain_graphs(fcat_med):
-    domain_pearson1 = domain_feat_freq.get_average(PEARSON1, 'Concept',
-        'correlation')
-    domain_pearson2 = domain_feat_freq.get_average(PEARSON2, 'Concept',
-        'correlation')
-    domain_matrix, domains, fcat_list = domain_feat_freq.get_feat_freqs(weights=fcat_med)
-    domain_feat_freq.render_graphs(GRAPH_DIR, domain_pearson1, domain_pearson2,
-        domains, domain_matrix, fcat_list)
-
-
 def get_values(input_file, c_string, value):
     concept_values = {}
     with open(input_file, 'rU') as csvfile:
@@ -251,69 +261,6 @@ def get_values(input_file, c_string, value):
                 row[value] = 0
             concept_values[row[c_string]] = float(row[value])
     return concept_values
-
-
-def get_fcat_conc_freqs(vocab, weights=None):
-    '''
-    @inputs:
-    - vocab: sorted list of concepts
-    - weights: {fcat: med}
-    '''
-    feature_cats = set()
-    concept_feats = {c: [] for c in vocab}
-    with open(FEATURES, 'r') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter='\t')
-        for row in reader:
-            if row["Concept"] in vocab:
-                concept_feats[row["Concept"]].append((row["BR_Label"], row["Prod_Freq"]))
-                feature_cats.add(row["BR_Label"])
-    fcat_list = sorted(list(feature_cats))
-
-    concept_matrix = np.zeros((len(vocab), len(fcat_list))) # rows: domains, columns: feature categories
-    for i in range(len(vocab)):
-        feats = concept_feats[vocab[i]] # list of tuples (feature category, production frequency)
-        for f in feats:
-            if weights and f[0] != "smell":
-                concept_matrix[i][fcat_list.index(f[0])] += weights[f[0]]*int(f[1])
-            else:
-                concept_matrix[i][fcat_list.index(f[0])] += int(f[1])
-    concept_totals = np.sum(concept_matrix, axis=1)
-    concept_matrix = concept_matrix/concept_totals[:,None]
-
-    return(concept_matrix, fcat_list)
-
-
-def produce_concept_graphs(fcat_med):
-    """
-    TODO: If we still want to use this function,
-    we should be sure to label the axes
-    """
-    concept_pearson1 = get_values(PEARSON1, 'Concept', 'correlation')
-    concept_pearson2 = get_values(PEARSON2, 'Concept', 'correlation')
-    assert concept_pearson1.keys() == concept_pearson2.keys()
-    vocab = sorted(concept_pearson1.keys())
-    concept_matrix, fcat_list = get_fcat_conc_freqs(vocab, weights=fcat_med)
-    print(concept_matrix)
-
-    xs = [concept_pearson1[concept] for concept in vocab]
-    ys = [concept_pearson2[concept] for concept in vocab]
-    concept_matrix = (concept_matrix - concept_matrix.min(axis=0)) / (concept_matrix.max(axis=0)
-        - concept_matrix.min(axis=0))
-    colormap = plt.get_cmap("cool")
-
-    # For each feature category, produce a scatter plot and use feature
-    # category metrics to color the points (as a sort of third dimension).
-    for j, fcat in enumerate(fcat_list):
-        print(fcat)
-        fig = plt.figure()
-        fig.suptitle(fcat+"-08-60-concepts-perc")
-
-        ax = fig.add_subplot(111)
-        c = colormap([concept_matrix[i, j] for i, concept in enumerate(vocab)])
-        ax.scatter(xs, ys, [], c)
-
-        fig_path = os.path.join(GRAPH_DIR, fcat)
-        fig.savefig(fig_path + '-08-60-concepts-perc')
 
 
 def plot_gaussian_contour(xs, ys, vars_xs, vars_ys):
@@ -701,9 +648,6 @@ def main():
 
     produce_unified_graph(vocab, features, feature_data, domain_concepts=domain_concepts)
     produce_unified_domain_graph(vocab, features, feature_data, domain_concepts=domain_concepts)
-
-    #produce_domain_graphs(fcat_med) # this calls functions in domain_feat_freq.py
-    #produce_concept_graphs(fcat_med) # this calls functions in here
 
 
 if __name__ == "__main__":
